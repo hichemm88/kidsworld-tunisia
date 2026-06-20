b19f4a07ac47172f834ce836cd7104d178db9b72
hichemm88 <hichemmathlouthi@gmail.com>
2026-06-20 17:01:49 +0100

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default function HomePage() {
         <Hero />
         <StatsBar />
         <CategoriesGrid />
-        <VillesGrid />
         <FeaturedListings />
+        <VillesGrid />
       </main>
     </>
   );
